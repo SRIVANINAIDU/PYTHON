d1860ecb2f7108a355c8d4cf54a56c1a458c7532
SRIVANINAIDU <srivanircr@gmail.com>
2025-10-03 19:08:12 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,20 @@
+list=[1,2,3,4,5,6,7,8,9,1,2,3,4,]
+k=set(list)
+ print(k)
+k.add(12)
+ print(k)
+ dist {   
+   "model":"1st model",
+    "year":2021,
+      "age":25
+ }
+print(type(dict))
+print(dict["model"])
+dict['city']="Hyderabad"
+ print(dict)
+def mom(a,b):
+    print(a+b)
+a=10
+b=20
+mom(a,b)
+mom(5,6)
